8a0ee07297cb66b2081e20510f09c30655a118bc
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2023-12-01 18:06:31 +0700

    hooks

--- 06-advanced/code/05-hooks-starting-code/hooks/hooks.test.js
+++ 06-advanced/code/05-hooks-starting-code/hooks/hooks.test.js
@@ -1,47 +1,57 @@
-import { it, expect } from 'vitest';
+import { it, expect, beforeAll, beforeEach, afterAll, afterEach } from "vitest";
+import { User } from "./hooks";
 
-import { User } from './hooks';
+const testEmail = "test@test.com";
+let user;
 
-it('should update the email', () => {
-  const testEmail = 'test@test.com';
-  const newTestEmail = 'test2@test.com';
-
-  const user = new User(testEmail);
-  user.updateEmail(newTestEmail);
-
-  expect(user.email).toBe(newTestEmail);
+beforeAll(() => {
+    user = new User(testEmail);
+    console.log("beforeAll()");
+});
+beforeEach(() => {
+    user = new User(testEmail);
+    console.log("beforeEach()");
+});
+afterEach(() => {
+    console.log("afterEach()");
+});
+afterAll(() => {
+    console.log("afterAll()");
 });
 
-it('should have an email property', () => {
-  const testEmail = 'test@test.com';
+// Với .concurrent bạn cũng có thể thực thi hành vi này bên trong các tệp riêng lẻ
+// (tức là các thử nghiệm nằm trong một và cùng một tệp được thực thi đồng thời).
 
-  const user = new User(testEmail);
+// Việc thực hiện đồng thời có thể giảm lượng thời gian mà các bài kiểm tra của bạn cần thực hiện.
+// Nhược điểm của việc thực thi đồng thời là các thử nghiệm thực hiện các thao tác trạng thái xung đột(toàn cầu) có thể gây trở ngại lẫn nhau.
 
-  expect(user).toHaveProperty('email');
+it.concurrent("should update the email", () => {
+    const newTestEmail = "test2@test.com";
+
+    user.updateEmail(newTestEmail);
+
+    expect(user.email).toBe(newTestEmail);
 });
 
-it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
-  expect(user.email).toBe(testEmail);
+it.concurrent("should have an email property", () => {
+    expect(user).toHaveProperty("email");
 });
 
-it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-  user.clearEmail();
-
-  expect(user.email).toBe('');
+it.concurrent("should store the provided email value", () => {
+    expect(user.email).toBe(testEmail);
 });
 
-it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
+it.concurrent("should clear the email", () => {
+    user.clearEmail();
 
-  const user = new User(testEmail);
-  user.clearEmail();
-
-  expect(user).toHaveProperty('email');
+    expect(user.email).toBe("");
 });
+
+it.concurrent(
+    "should still have an email property after clearing the email",
+    () => {
+        user.clearEmail();
+
+        expect(user).toHaveProperty("email");
+    },
+);
