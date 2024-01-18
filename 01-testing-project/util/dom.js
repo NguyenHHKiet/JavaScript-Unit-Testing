1a33565bccef01adcfb6baa1e53323ac8faaa1f0
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2024-01-18 15:16:14 +0700

    deleted

--- 01-testing-project/util/dom.js
+++
@@ -1,8 +0,0 @@
-export function showError(message) {
-    const errorContainerElement = document.getElementById("errors");
-    const errorMessageElement = document.createElement("p");
-    errorMessageElement.textContent = message;
-    errorMessageElement.className = "badge text-bg-danger m-0 mt-2";
-    errorContainerElement.innerHTML = "";
-    errorContainerElement.append(errorMessageElement);
-}
